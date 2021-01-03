99a961f5b2d71beebf3681ca2aac940f5e679122
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-01-04 01:20:24 +0530

    adding notes dynamically

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes.js";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((data) => (
+        <Note key={data.key} title={data.title} content={data.content} />
+      ))}
       <Footer />
     </div>
   );
