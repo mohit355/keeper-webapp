94bf649a0a629e748e8601887e2b446e055aa5f1
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-01-04 01:59:04 +0530

    adding delete note and add note button functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
-import notes from "../notes.js";
+import CreateArea from "./CreateArea";
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  };
+
+  const deleteNode = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div>
       <Header />
-      {notes.map((data) => (
-        <Note key={data.key} title={data.title} content={data.content} />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNode}
+        />
       ))}
       <Footer />
     </div>
